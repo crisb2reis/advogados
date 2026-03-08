575a4fa9765ace1d316cced77ad0ed1b6ba4d0a9
crisb2reis <crisb2reis@hotmail.com>
2026-03-07 22:02:11 -0300

    Configure GitHub Pages and automatic deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/advogados',
+  assetPrefix: '/advogados',
   images: {
     unoptimized: true,
   },
